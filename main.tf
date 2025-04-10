f62a32d844f311b6c13ec63e8cfaa43357118b3f
eriknathan <eriknathan.contato@gmail.com>
2025-04-10 11:49:52 -0300

    adding module sg

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "s3-erik-tf"          
+    bucket = "s3-erik-tf"
     key    = "logiteste/terraform.tfstate"
     region = "us-east-1"
   }
@@ -18,8 +18,8 @@ module "vpc" {
   vpc_cidr          = "10.1.0.0/16"
   subnet_cidr       = "10.1.1.0/24"
   availability_zone = "us-east-1a"
-  project_name       = "logitest"
-  tags               = local.tags  
+  project_name      = "logitest"
+  tags              = local.tags
 }
 
 module "ec2_instance" {
@@ -30,7 +30,32 @@ module "ec2_instance" {
   subnet_id          = module.vpc.subnet_id
   project_name       = "logitest"
   disk_size          = 40
-  tags               = local.tags  
+  security_group_ids = [module.sg.security_group_id]
+  tags               = local.tags
+}
+
+
+module "sg" {
+  source       = "git::https://github.com/eriknathan/terraform-modules.git//modules/security-group?ref=main"
+  project_name = "logitest"
+  vpc_id       = module.vpc.vpc_id
+  description  = "Permitir acesso SSH e HTTP"
+  tags         = local.tags
+
+  ingress_rules = [
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["201.71.62.130/32"]
+    },
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
 }
 
 # --------------------------------------------------------------------------------
